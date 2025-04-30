bc14ed703d76e2a2968ee18f0d5c44dbcea6bdfb
Ricardo Sobjak <ricardosobjak@gmail.com>
2025-04-30 06:33:37 -0300

    Cognito

---
+++ springboot/api-cognito/src/main/java/br/edu/utfpr/tac/cognito/CognitoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.tac.cognito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitoApplication.class, args);
+	}
+
+}
